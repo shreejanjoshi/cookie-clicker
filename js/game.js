4ac652a99dc29cb785590ecdc47b7a86dfe95ff7
Sushanta Pyakurel <sushanta@Sushantas-MacBook-Pro.local>
2021-10-12 10:21:44 +0200

    Almost Done

--- js/game.js
+++ js/game.js
@@ -1,16 +1,18 @@
+(function() {
 
   var score = 0;
   var type = "default";
   var multiplier_cost = 10;
-  var bonus_cost = 20;
-  var autoclicker_cost = 30;
+  var multiplier_purchase = 0;
+  var bonus_cost = 30;
+  var bonus_purchase = 0;
+  var autoclicker_cost = 20;
+  var autoclicker_purchase = 0;
 
 
   const clicker = document.getElementById('clicker');
   const target = document.getElementById('score_count');
-  const setdefault = document.getElementById('default');
   const multiplier = document.getElementById('multiplier');
-  const reset = document.getElementById('reset');
   const autoclicker = document.getElementById('autoclicker');
   const bonus = document.getElementById('bonus');
 
@@ -20,10 +22,10 @@
       score ++;
     }
     if(type == 'multiplier') {
-      score = score + 2;
+      score = score + (multiplier_purchase*2);
     }
     if(type == 'bonus') {
-      score = score + 5;
+      score = score + (bonus_purchase*3);
     }
 
     loadscore();
@@ -35,16 +37,22 @@
   }
 
   clicker.addEventListener('click', main);
-  setdefault.addEventListener('click', function() {
-    type = "default";
-  });
+  // setdefault.addEventListener('click', function() {
+  //   type = "default";
+  // });
   multiplier.addEventListener('click', function() {
     if(score >= multiplier_cost) {
       score = score - multiplier_cost;
       loadscore();
       type = "multiplier";
+      multiplier_purchase ++;
       multiplier_cost = multiplier_cost + 10;
       document.getElementById('multiplier_cost').innerHTML = multiplier_cost;
+      multiplier_purchase_count = "";
+      for(i=0;i<multiplier_purchase; i++) {
+        multiplier_purchase_count = multiplier_purchase_count + `| `;
+      }
+      document.getElementById('multiplier_purchase_count').innerHTML = multiplier_purchase_count;
     }
     else {
       alert(`you can't buy this option`);
@@ -54,11 +62,19 @@
   autoclicker.addEventListener('click', function() {
     if(score >= autoclicker_cost) {
       score = score - autoclicker_cost;
-      loadscore();
+      autoclicker_cost = autoclicker_cost + 20;
+      autoclicker_purchase ++;
+      document.getElementById('autoclicker_cost').innerHTML = autoclicker_cost;
+      multiplier_cost = multiplier_cost + 20;
       setInterval(function() {
-      score ++;
+      score = score + autoclicker_purchase;
       loadscore();
-      }, 1000)
+      autoclicker_purchase_count = "";
+      for(i=0;i<autoclicker_purchase; i++) {
+        autoclicker_purchase_count = autoclicker_purchase_count + `| `;
+      }
+      document.getElementById('autoclicker_purchase_count').innerHTML = autoclicker_purchase_count;
+    }, 1000);
     }
     else {
       alert(`you can't buy this option`);
@@ -68,20 +84,38 @@
   bonus.addEventListener('click', function() {
     if(score >= bonus_cost) {
         score = score - bonus_cost;
+        bonus_cost = bonus_cost + 20;
+        bonus_purchase ++;
+        document.getElementById('bonus_cost').innerHTML = bonus_cost;
         loadscore();
         type = "bonus";
         setTimeout(function() {
           type =  "default"
-        },3000)
+        },3000);
+        bonus_purchase_count = "";
+        for(i=0;i<bonus_purchase; i++) {
+          bonus_purchase_count = bonus_purchase_count + `| `;
+        }
+        document.getElementById('bonus_purchase_count').innerHTML = bonus_purchase_count;
       }
       else {
         alert(`you can't buy this option`);
       }
     });
 
-  reset.addEventListener('click', function() {
-    score = 0;
-    type = "default";
-    multiplier_cost = 10;
-    loadscore();
-  });
+    var elements = document.getElementsByClassName('btn');
+    Array.from(elements).forEach(function(element) {
+      element.addEventListener('mouseover', function() {
+        var id = this.getAttribute('id');
+         var selector = `more_info_` + id;
+         document.getElementById(selector).classList.remove('hidden');
+      });
+    });
+    Array.from(elements).forEach(function(element) {
+      element.addEventListener('mouseleave', function() {
+        var id = this.getAttribute('id');
+         var selector = `more_info_` + id;
+         document.getElementById(selector).classList.add('hidden');
+      });
+    });
+  })();
